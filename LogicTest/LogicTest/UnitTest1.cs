fa19ac89a2b45a6ae8813858d70d64e1496368b5
Hébert, François <francois.hebert@elekta.com>
2016-09-21 09:43:12 -0400

    Testing jenkins

--- LogicTest/LogicTest/UnitTest1.cs
+++ LogicTest/LogicTest/UnitTest1.cs
@@ -29,6 +29,7 @@ namespace LogicTest
             StreamLogic messageLogic = new StreamLogic();
             string message;
             string expected = "Hello You!!!";
+            // Add comment for jenkins
 
             // Act
             message = messageLogic.getMessage();
